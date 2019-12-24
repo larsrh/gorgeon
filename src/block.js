b1381871e615aa862b79573b1885531d2ca0e4d9
FND <fnd@localhost.localdomain>
2019-12-24 13:11:34 +0100

    implemented block rendering
    only using dummy transforms for now

--- src/block.js
+++ src/block.js
@@ -1,3 +1,5 @@
+import { abort } from "./util.js";
+
 export default class ContentBlock {
 	constructor(format, params, content) {
 		Object.assign(this, {
@@ -5,6 +7,18 @@ export default class ContentBlock {
 			params: params || {},
 			content
 		});
+	}
+
+	// `transforms` is a `{ format: transform }` object; the matching transform
+	// is invoked with `context`
+	// `context.origin` is the original content's file path
+	render(transforms, context) {
+		let { format } = this;
+		let transform = transforms[format];
+		if(!transform) {
+			abort(`ERROR: unknown format \`${format}\` for \`${context.origin}\``);
+		}
+		return transform(this.content, this.params, context);
 	}
 
 	toJSON() {
