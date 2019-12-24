b40b844e7812a7161887cfc093193ed8c0e3856f
FND <fnd@localhost.localdomain>
2019-12-24 13:11:34 +0100

    implemented block rendering
    only using dummy transforms for now

--- test/util.js
+++ test/util.js
@@ -1,3 +1,29 @@
 import path from "path";
 
 export let fixturePath = name => path.resolve(__dirname, "fixtures", name);
+
+export class BufferedStream {
+	constructor() {
+		this._buffer = [];
+	}
+
+	write(msg) {
+		this._buffer.push(msg);
+	}
+
+	read(reset) {
+		let { _buffer } = this;
+		if(reset) {
+			this.reset();
+		}
+		return _buffer.flat().join(" ");
+	}
+
+	reset() {
+		this._buffer = [];
+	}
+}
+
+export function raise(msg) {
+	throw new Error(msg);
+}
