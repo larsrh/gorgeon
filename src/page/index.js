bdbfea1caaa5f597c65dbdf7adbf3e49ca68b3ad
FND <fnd@localhost.localdomain>
2020-01-02 11:52:51 +0100

    implemented layout customization
    in the process, added test for async transforms

--- src/page/index.js
+++ src/page/index.js
@@ -1,11 +1,12 @@
 import parse from "./parser.js";
+import { abort } from "../util.js";
 
 export default class ContentPage {
 	constructor(filepath) {
 		this.filepath = filepath;
 	}
 
-	async render(layout, transforms) {
+	async render(layouts, transforms) {
 		let { filepath } = this;
 		let { meta, blocks } = await parse(filepath);
 
@@ -14,6 +15,16 @@ export default class ContentPage {
 			map(blk => blk.render(transforms, context)));
 		let html = blocks.join("");
 
+		let { layout } = meta;
+		if(layout) {
+			let id = layout;
+			layout = layouts[id];
+			if(!layout) {
+				abort(`ERROR: unknown layout \`${id}\` in \`${filepath}\``);
+			}
+		} else {
+			layout = layouts.default;
+		}
 		return layout(meta, html);
 	}
 }
