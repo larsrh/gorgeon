f0b1cc515e588fdb04c036a47a9196139a18bdb1
FND <fnd@localhost.localdomain>
2020-01-02 14:39:50 +0100

    added option for transforms to create secondary files

---
+++ test/fixtures/subs.md
@@ -0,0 +1,22 @@
+title: Hello World
+lang: en
+
+lorem ipsum
+dolor sit amet
+
+```html
+<figure>
+    <canvas width="100" height="100">blank canvas</canvas>
+    <figcaption>a blank canvas</figcaption>
+</figure>
+```
+
+consectetur adipisicing elit,
+sed do eiusmod tempor
+
+```html
+<figure>
+    <canvas width="10" height="10">empty canvas</canvas>
+    <figcaption>an empty canvas</figcaption>
+</figure>
+```
--- test/test_site.js
+++ test/test_site.js
@@ -43,6 +43,56 @@ md:52
 	await fs.rmdir(rootDir, { recursive: true });
 });
 
+test("transforms' secondary files", async () => {
+	let rootDir = await mkdtemp();
+	let site = new WebSite(rootDir, {
+		default: (meta, html) => `----\n${html.trim()}\n----`
+	}, {
+		md: txt => `md:${txt.length}\n`,
+		html: async (code, params, context) => {
+			let filename = await context.createSub("html", code);
+			return `<a href="${filename}">${filename}</a>\n`;
+		}
+	});
+
+	let filepath = fixturePath("subs.md");
+	await site.createPage(filepath);
+
+	let pageDir = path.join(rootDir, "subs");
+	let readFile = filename => {
+		let filepath = path.join(pageDir, filename);
+		return fs.readFile(filepath, "utf8");
+	};
+
+	let html = await readFile("index.html");
+	assertSame(html, `
+----
+md:27
+<a href="0.html">0.html</a>
+md:53
+<a href="1.html">1.html</a>
+----
+	`.trim());
+
+	html = await readFile("0.html");
+	assertSame(html, `
+<figure>
+    <canvas width="100" height="100">blank canvas</canvas>
+    <figcaption>a blank canvas</figcaption>
+</figure>
+	`.trim());
+
+	html = await readFile("1.html");
+	assertSame(html, `
+<figure>
+    <canvas width="10" height="10">empty canvas</canvas>
+    <figcaption>an empty canvas</figcaption>
+</figure>
+	`.trim());
+
+	await fs.rmdir(rootDir, { recursive: true });
+});
+
 async function mkdtemp() {
 	return fs.mkdtemp(path.join(os.tmpdir(), "gorgeon-"));
 }
