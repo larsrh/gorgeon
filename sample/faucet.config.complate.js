5854047625d0c63131b52e930a846e091a387072
FND <fnd@localhost.localdomain>
2020-02-04 15:39:43 +0100

    created convenience abstraction for complate layout renderer
    this further reduces awkward/distracting/confusing boilerplate and is
    mostly required due to the workarounds mentioned inline

--- sample/faucet.config.complate.js
+++ sample/faucet.config.complate.js
@@ -2,32 +2,21 @@
 
 require = require("esm")(module); // eslint-disable-line no-global-assign
 let renderMarkdown = require("../src/markdown.js").default;
-let makeTransform = require("../src/complate/index.js").default;
+let { makeRenderer, makeTransform } = require("../src/complate/index.js");
 let path = require("path");
-
-let renderComponent = makeTransform(__dirname);
 
 module.exports = {
 	watchDirs: ["./content"],
 	gorgeon: [{
 		pages: ["./content/index.md", "./content/components.md"],
 		targetDir: "./dist",
-		layouts: { default: renderDocument },
+		layouts: {
+			default: makeRenderer("Document", "./components.jsx", __dirname)
+		},
 		transforms: {
 			md: (md, params, context) => renderMarkdown(md, { fragIDs: true }),
-			complate: renderComponent
+			complate: makeTransform(__dirname)
 		}
 	}],
 	plugins: [path.resolve(__dirname, "../src/faucet.js")]
 };
-
-async function renderDocument(meta, html) {
-	// XXX: workarounds for Rodunj's lack of support for raw HTML and document types
-	let placeholder = "%CONTENT%";
-	let _html = await renderComponent(`
-import { Document } from "./components.jsx";
-
-<Document {...context}>${placeholder}</Document>
-	`, meta);
-	return "<!DOCTYPE html>\n" + _html.replace(placeholder, html);
-}
