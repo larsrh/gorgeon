f7fe27f3894f4701b82bd19abd4c1c62c5acd88c
FND <fnd@localhost.localdomain>
2020-02-04 15:39:43 +0100

    created convenience abstraction for complate layout renderer
    this further reduces awkward/distracting/confusing boilerplate and is
    mostly required due to the workarounds mentioned inline

--- src/complate/index.js
+++ src/complate/index.js
@@ -1,11 +1,39 @@
 import _Bundle from "./bundling.js";
-import { load } from "../util.js";
+import { load, abort } from "../util.js";
 import vm from "vm";
 
-export default function makeTransform(referenceDir) {
+let DOCTYPE = "<!DOCTYPE html>\n";
+let PLACEHOLDER = `%~${Math.random().toString().substr(2)}~%`;
+let LAYOUT = (component, filepath, placeholder) => `
+import { ${component} } from "${filepath}";
+
+<${component} {...context}>${placeholder}</${component}>
+`;
+
+// generates a layout function for the given `component` name, to be imported
+// from `filepath`, relative to `referenceDir`
+// XXX: workaround for Rodunj's lack of support for document types and raw HTML
+export function makeRenderer(component, filepath, referenceDir) {
+	if(/[^a-zA-Z0-9]/.test(component)) { // primitive heuristic
+		abort("ERROR: layout component names must be alphanumeric; " +
+				`\`${component}\` is invalid`);
+	}
+	if(filepath.includes('"')) {
+		abort(`ERROR: invalid module path \`${component}\``);
+	}
+	let renderComponent = makeTransform(referenceDir);
+	let jsx = LAYOUT(component, filepath, PLACEHOLDER);
+
+	return async function renderLayout(meta, html) {
+		let _html = await renderComponent(jsx, meta);
+		return DOCTYPE + _html.replace(PLACEHOLDER, html);
+	};
+}
+
+export function makeTransform(referenceDir) {
 	let bundle = new Bundle(referenceDir);
-	return (jsx, params, context) => bundle.renderString(jsx, "snippet.jsx",
-			{ context: params });
+	return (jsx, params, context) => bundle.
+		renderString(jsx, "snippet.jsx", { context: params });
 }
 
 export class Bundle extends _Bundle {
