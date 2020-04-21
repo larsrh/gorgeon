390a6eccf76070344320850780f9825466c8ee7b
FND <fnd@localhost.localdomain>
2020-04-21 08:43:24 +0200

    simplified complate sample
    faucet-core now permits async configuration
    
    still a little awkward and boilerplate-y though (though in part that's
    due to Rodunj's whitespace preservation)

--- samples/faucet.config.complate.js
+++ samples/faucet.config.complate.js
@@ -9,26 +9,21 @@ let COMPLATE = makeTransform(__dirname);
 let LAYOUT = path.resolve(__dirname, "layout.jsx");
 LAYOUT = fs.readFile(LAYOUT, "utf8");
 
-module.exports = {
+module.exports = Promise.all([COMPLATE, LAYOUT]).then(([complate, layout]) => ({
 	watchDirs: ["./content"],
 	gorgeon: [{
 		pages: ["./content/index.md", "./content/components.md"],
 		targetDir: "./dist",
 		layouts: {
 			default: async (meta, html) => {
-				html = await render(await LAYOUT, { params: meta, html });
+				html = await complate(layout, { params: meta, html });
 				return html.trim();
 			}
 		},
 		transforms: {
 			md: (md, params, context) => renderMarkdown(md, { fragIDs: true }),
-			complate: render
+			complate
 		}
 	}],
 	plugins: ["faucet-pipeline-gorgeon"]
-};
-
-async function render(jsx, params, context) {
-	let _render = await COMPLATE;
-	return _render(jsx, params, context);
-}
+}));
